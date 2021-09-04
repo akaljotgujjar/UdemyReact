1eb6c2ffd60469c07ff381afcde04cc41b6adbd7
Akaljot Gujjar <akaljotgujjar@gmail.com>
2021-09-04 13:50:11 -0700

    started on project

---
+++ src/components/Expenses/Expense.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import "./Expense.css";
+import ExpensesFilter from "./ExpensesFilter";
+
+const Expenses = ({ items }) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        <ExpenseItem
+          title={items[0].title}
+          amount={items[0].amount}
+          date={items[0].date}
+        />
+        <ExpenseItem
+          title={items[1].title}
+          amount={items[1].amount}
+          date={items[1].date}
+        />
+        <ExpenseItem
+          title={items[2].title}
+          amount={items[2].amount}
+          date={items[2].date}
+        />
+        <ExpenseItem
+          title={items[3].title}
+          amount={items[3].amount}
+          date={items[3].date}
+        />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = ({ date, title, amount }) => {
+  const [expenseTitle, setExpenseTitle] = useState(title);
+
+  const clickHandler = () => {
+    setExpenseTitle('Updated!');
+    console.log(expenseTitle);
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={date} />
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
